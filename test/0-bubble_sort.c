1ca18c1a5be111510d692e51c9032b2bb1aaa0db
emma-obelenge <obeleemmanuel@gmail.com>
2024-02-20 20:11:04 +0000

    add test directory for sorting algorithms project

---
+++ test/0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Implementing the bubble sort algorithm
+ * @array: array of integers to be sorted
+ * @size: size of the array to be sorted
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp;
+	int swap;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				swap = 1;
+			}
+			if (swap == 1)
+			{
+				print_array(array, size);
+				swap = 0;
+			}
+		}
+	}
+}
---
+++ test/1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Function for sorting linked list
+ * using the insertion sort algorithm
+ * @list: double pointer to the linked list to be sorted
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *pos_ptr, *change, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	pos_ptr = (*list)->next;
+	while (pos_ptr != NULL)
+	{
+		change = pos_ptr;
+		temp = pos_ptr->prev;
+		while (change->n < temp->n)
+		{
+			pos_ptr = pos_ptr->next;
+			temp->next = change->next;
+			change->next->prev = temp;
+			change->next = temp;
+			change->prev = temp->prev;
+			temp->prev = change;
+			if (change->prev != NULL)
+				temp = change->prev;
+		}
+		while (change->prev != NULL)
+		{
+			change = change->prev;
+		}
+		*list = change;
+		print_list(*list);
+	}
+}
